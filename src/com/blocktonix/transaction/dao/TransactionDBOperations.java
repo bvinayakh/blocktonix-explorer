0ede0583ba276a2ad29728fd94f6944f9661b1f7
Sree Vinayak Balasubramaniam <svinayak@8kmiles.com>
2021-04-06 12:28:48 -0700

    handle existing transactions and contracts in DB

--- src/com/blocktonix/transaction/dao/TransactionDBOperations.java
+++ src/com/blocktonix/transaction/dao/TransactionDBOperations.java
@@ -12,6 +12,7 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.ParameterExpression;
 import javax.persistence.criteria.Root;
 import org.apache.commons.lang3.StringUtils;
+import org.hibernate.exception.ConstraintViolationException;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.methods.response.EthBlock.TransactionObject;
 import org.web3j.protocol.core.methods.response.Transaction;
@@ -70,67 +71,77 @@ public class TransactionDBOperations
     return parentNode;
   }
 
-  public String storeTransaction(TransactionObject transaction) throws Exception
+  public String storeTransaction(TransactionObject transaction)
   {
-    TransactionDao dao = new TransactionDao();
-    dao.blockHash = transaction.getBlockHash();
-    dao.blockNumber = String.valueOf(transaction.getBlockNumber());
-    dao.from = String.valueOf(transaction.getFrom());
-    dao.gas = String.valueOf(transaction.getGas());
-    dao.gasPrice = String.valueOf(transaction.getGasPrice());
-    dao.hash = transaction.getHash();
-    String transactionInput = transaction.getInput();
-    dao.input = transactionInput;
-    // check for contract transfer within transaction
-    if (transactionInput.startsWith("0xa9059cbb"))
+    try
     {
-      String contractAddress = transaction.getTo();
-      transactionInput = StringUtils.replace(transactionInput, "0xa9059cbb", "");
-      String transferedToAddress = "0x" + StringUtils.substring(transactionInput, 24, 64);
-      String transferAmount = StringUtils.substring(transactionInput, 64, 128);
-      ObjectNode contractNode = mapper.createObjectNode();
-      JsonNode contractInfoNode = contractOps.getContractInfo(contractAddress);
-      contractNode.putPOJO("Decimals", contractInfoNode.get("Decimals"));
-      contractNode.putPOJO("Symbol", contractInfoNode.get("Symbol"));
-      contractNode.putPOJO("TotalSupply", contractInfoNode.get("TotalSupply"));
-      contractNode.putPOJO("Name", contractInfoNode.get("Name"));
-      contractNode.putPOJO("ContractAddress", contractAddress);
-      contractNode.putPOJO("TransferFrom", transaction.getFrom());
-      contractNode.putPOJO("TransferTo", transferedToAddress);
-      // convert amount to readable decimal number
-      String xferAmountString = String.valueOf(Numeric.toBigInt(transferAmount));
-      Integer decimalsInt = Integer.valueOf(contractInfoNode.get("Decimals").asText());
-      String preRoundedAmount = calculateDecimalPlaces(transaction.getHash(), contractAddress, xferAmountString, '.', decimalsInt);
-      String amountRounded = preRoundedAmount.substring(0, preRoundedAmount.indexOf('.') + 3);
-      // contractNode.putPOJO("Amount", calculateDecimalPlaces(transaction.getHash(), contractAddress,
-      // xferAmountString, '.', decimalsInt));
-      contractNode.putPOJO("Amount", amountRounded);
-      contractNode.putPOJO("TransactionHash", transaction.getHash());
-      contractNode.putPOJO("Block", transaction.getBlockNumber());
-      // search for contractAbi in database. if not found then get from etherscan
-      String contractAbi = contractDbOps.getContractAbi(contractAddress);
-      if (contractAbi == null)
+      TransactionDao dao = new TransactionDao();
+      dao.blockHash = transaction.getBlockHash();
+      dao.blockNumber = String.valueOf(transaction.getBlockNumber());
+      dao.from = String.valueOf(transaction.getFrom());
+      dao.gas = String.valueOf(transaction.getGas());
+      dao.gasPrice = String.valueOf(transaction.getGasPrice());
+      dao.hash = transaction.getHash();
+      String transactionInput = transaction.getInput();
+      dao.input = transactionInput;
+      // check for contract transfer within transaction
+      if (transactionInput.startsWith("0xa9059cbb"))
       {
-        contractAbi = contractOps.getContractABI(contractAddress);
-        contractDbOps.storeContractAbi(contractAddress, contractInfoNode.get("Symbol").asText(), contractAbi);
+        String contractAddress = transaction.getTo();
+        transactionInput = StringUtils.replace(transactionInput, "0xa9059cbb", "");
+        String transferedToAddress = "0x" + StringUtils.substring(transactionInput, 24, 64);
+        String transferAmount = StringUtils.substring(transactionInput, 64, 128);
+        ObjectNode contractNode = mapper.createObjectNode();
+        JsonNode contractInfoNode = contractOps.getContractInfo(contractAddress);
+        contractNode.putPOJO("Decimals", contractInfoNode.get("Decimals"));
+        contractNode.putPOJO("Symbol", contractInfoNode.get("Symbol"));
+        contractNode.putPOJO("TotalSupply", contractInfoNode.get("TotalSupply"));
+        contractNode.putPOJO("Name", contractInfoNode.get("Name"));
+        contractNode.putPOJO("ContractAddress", contractAddress);
+        contractNode.putPOJO("TransferFrom", transaction.getFrom());
+        contractNode.putPOJO("TransferTo", transferedToAddress);
+        // convert amount to readable decimal number
+        String xferAmountString = String.valueOf(Numeric.toBigInt(transferAmount));
+        Integer decimalsInt = Integer.valueOf(contractInfoNode.get("Decimals").asText());
+        String preRoundedAmount = calculateDecimalPlaces(transaction.getHash(), contractAddress, xferAmountString, '.', decimalsInt);
+        String amountRounded = preRoundedAmount.substring(0, preRoundedAmount.indexOf('.') + 3);
+        // contractNode.putPOJO("Amount", calculateDecimalPlaces(transaction.getHash(), contractAddress,
+        // xferAmountString, '.', decimalsInt));
+        contractNode.putPOJO("Amount", amountRounded);
+        contractNode.putPOJO("TransactionHash", transaction.getHash());
+        contractNode.putPOJO("Block", transaction.getBlockNumber());
+        // search for contractAbi in database. if not found then get from etherscan
+        String contractAbi = contractDbOps.getContractAbi(contractAddress);
+        if (contractAbi == null)
+        {
+          contractAbi = contractOps.getContractABI(contractAddress);
+          contractDbOps.storeContractAbi(contractAddress, contractInfoNode.get("Symbol").asText(), contractAbi);
+        }
+        contractNode.putPOJO("ABI", contractAbi);
+        // contractNode.putPOJO("ABI", contractOps.getContractABI(contractAddress));
+        contractDbOps.storeContract(contractNode);
       }
-      contractNode.putPOJO("ABI", contractAbi);
-      // contractNode.putPOJO("ABI", contractOps.getContractABI(contractAddress));
-      contractDbOps.storeContract(contractNode);
-
+      dao.nonce = String.valueOf(transaction.getNonce());
+      dao.r = transaction.getR();
+      dao.s = transaction.getS();
+      dao.to = transaction.getTo();
+      dao.transactionIndex = String.valueOf(transaction.getTransactionIndex());
+      dao.v = String.valueOf(transaction.getV());
+      dao.value = String.valueOf(transaction.getValue());
+      entitymanager.getTransaction().begin();
+      entitymanager.persist(dao);
+      entitymanager.getTransaction().commit();
+      // entitymanager.close();
+      System.out.println("stored transaction " + transaction.getHash() + " from block " + transaction.getBlockNumber());
     }
-    dao.nonce = String.valueOf(transaction.getNonce());
-    dao.r = transaction.getR();
-    dao.s = transaction.getS();
-    dao.to = transaction.getTo();
-    dao.transactionIndex = String.valueOf(transaction.getTransactionIndex());
-    dao.v = String.valueOf(transaction.getV());
-    dao.value = String.valueOf(transaction.getValue());
-    entitymanager.getTransaction().begin();
-    entitymanager.persist(dao);
-    entitymanager.getTransaction().commit();
-    // entitymanager.close();
-    System.out.println("stored transaction " + transaction.getHash() + " from block " + transaction.getBlockNumber());
+    catch (ConstraintViolationException constraintException)
+    {
+      System.err.println("Entry exists for Contract " + transaction.getTo() + " in transaction " + transaction.getHash());
+    }
+    catch (Exception e)
+    {
+      System.err.println("Error in Contract ABI" + e.getMessage());
+    }
     return transaction.getHash();
   }
 
