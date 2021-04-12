2274ef4891325aaa3a239d133178863b22fafc2a
Sree Vinayak Balasubramaniam <svinayak@8kmiles.com>
2021-04-11 20:36:14 -0700

    wallet update

--- src/com/blocktonix/wallet/dao/WalletDBOperations.java
+++ src/com/blocktonix/wallet/dao/WalletDBOperations.java
@@ -44,8 +44,7 @@ public class WalletDBOperations
     dao.walletBalanceAt = TimeUtil.getCurrentUTCDateWithTimeZone();
     dao.transactionHash = txnHash;
     session.beginTransaction();
-    session.delete(dao);
-    session.save(dao);
+    session.saveOrUpdate(dao);
     session.getTransaction().commit();
     logger.info("Wallet ETH Balance updated for " + walletAddress);
   }
