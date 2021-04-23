9c09b836fead2c20f3f61141746da3e5aa8924df
Sree Vinayak Balasubramaniam <svinayak@8kmiles.com>
2021-04-23 12:21:45 -0700

    updates

--- src/com/blocktonix/main/ForwardSync.java
+++ src/com/blocktonix/main/ForwardSync.java
@@ -2,27 +2,21 @@ package com.blocktonix.main;
 
 import java.io.IOException;
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.web3j.protocol.core.methods.response.EthBlock.Block;
 import com.blocktonix.block.BlockOperations;
-import com.blocktonix.block.BlockTask;
 import com.blocktonix.block.dao.BlockDBOperations;
-import com.blocktonix.utils.Constants;
 
 public class ForwardSync implements Runnable
 {
   public static final Logger logger = LoggerFactory.getLogger(ForwardSync.class);
-
+  
   private List<BigInteger> forwardBlockNumbersList = null;
+
+  private BlockOperations blockOps = new BlockOperations();
+  private BlockDBOperations blockDbOperations = new BlockDBOperations();
 
   public ForwardSync(List<BigInteger> blocksList)
   {
@@ -34,39 +28,17 @@ public class ForwardSync implements Runnable
   {
     for (BigInteger blockNumber : forwardBlockNumbersList)
     {
-      logger.info("Fetching forward block : " + blockNumber);
-      processBlock(blockNumber);
+      Block block;
       try
       {
-        Thread.sleep(3000);
+        logger.info("Fetching forward block : "+ blockNumber);
+        block = blockOps.getBlock(blockNumber).getBlock();
+        blockDbOperations.storeBlock(block);
       }
-      catch (InterruptedException e)
+      catch (IOException e)
       {
-        System.err.println("Error while waiting for new block creation " + e.getMessage());
+        System.err.println("Exception in getting block information " + e.getMessage());
       }
     }
-  }
-
-  private void processBlock(BigInteger blockNumber)
-  {
-    ExecutorService executor = Executors.newFixedThreadPool(20);
-    Collection<Callable<String>> callables = new ArrayList<Callable<String>>();
-    callables.add(new BlockTask(Constants.web3, blockNumber));
-    List<Future<String>> futures;
-    try
-    {
-      futures = executor.invokeAll(callables);
-      for (Future<String> future : futures)
-      {
-        String responseContent = future.get();
-      }
-      futures.clear();
-    }
-    catch (InterruptedException | ExecutionException e)
-
-    {
-      logger.error("Execution Error while processing block " + e.getMessage());
-    }
-    executor.shutdown();
   }
 }
